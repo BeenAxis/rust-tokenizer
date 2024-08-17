baad50ab06a2b3a9a58a5a61a1d25dc2bc87374e
BeenAxis <gkhtecla@gmail.com>
2024-08-18 01:26:02 +0300

    feat: replacers

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 use std::fs::File;
-use std::collections::LinkedList;
-use std::fmt::Debug;
+use std::ops::Index;
 use std::time::Instant;
-
-use utils::read_lines;
-
-use crate::Token::StrLit;
-use crate::utils::linkedlist::LinkedHashList;
+use crate::stacktoken::{TestCustomTokens, Token, TokenArgs, TokenBuilder};
 
 mod utils;
+mod stacktoken;
 
 fn main() {
-    let mut hl2:LinkedList<String> = LinkedList::new();
+    /*let mut hl2:LinkedList<String> = LinkedList::new();
     hl2.push_back(String::from("A"));
     hl2.push_back(String::from("B"));
     println!("{hl2:?}");
@@ -20,179 +16,69 @@ fn main() {
         hl.push(format!("A with {}", i));
     }
     let el = Instant::now();
-    hl.replace(1000..5000, String::from("NEW"));
-    hl.replace(950..1050, String::from("NEW"));
-    hl.replace(10000..95000, String::from("NEW"));
-    //hl.reindex();
-    //0..100000
-    //0..[500 1000]..100000
-    let el = el.elapsed();
+    hl.replace(1000..10000, String::from("NEW"));
+    hl.replace(1000..50000, String::from("NEW"));
+    hl.replace(1000..20000, String::from("NEW"));*/
+    
+    /*let el = el.elapsed();
     println!("{hl:?}");
-    println!("Replace: {el:?} {}", hl.len());
-    /*let mut tkb: TokenBuilder<TestCustomToken> = TokenBuilder::new();
+    println!("Replace: {el:?} {}", hl.len());*/
+
+    let mut tkb: TokenBuilder<TestCustomTokens> = TokenBuilder::new();
     tkb.from_file("test.mc");
+    let els = Instant::now();
+    /*let founded = tkb.found("(\"");
+    tkb.replace(founded.as_slice(), |_| Token::Custom(TestCustomTokens::StartParen));
+    let founded = tkb.found("\")");
+    tkb.replace(founded.as_slice(), |_| Token::Custom(TestCustomTokens::EndParen));*/
+    let range = tkb.found_range(Token::Char('"'), Token::Char('"'));
 
-    let t = Instant::now();
+    tkb.replace(range.as_slice(), |list| {
+        Token::StrLit(list.scan_string())
+    });
 
-    let founded = tkb.found("t");
-    let del_f = t.elapsed();
-    println!("{founded:?}");
-    tkb.replace(founded.as_slice(), Token::Custom(TestCustomToken::MethodCall("test".into())));
-    //tkb.replace(a.as_slice(), Token::Custom(TestCustomToken::MethodCall("test".into())));
-    let del = t.elapsed();
-    tkb.debug();
-    println!("Elapsed: {del:?}");
-    println!("Elapsed del f: {del_f:?}");*/
-}
+    let founded = tkb.found("compileOnly");
+    tkb.replace(founded.as_slice(), |_| Token::Custom(TestCustomTokens::CompileOnly));
 
-pub trait TokenHolder: Clone + Debug + PartialEq {}
+    let range = tkb.found_range(Token::Char('('), Token::Char(')'));
+    //println!("Range: {range:#?}");
+    tkb.replace(range.as_slice(), |mut list| {
+        list.remove_backs();
+        Token::Custom(TestCustomTokens::Group(list))
+    });
 
-pub trait Tokens<HOLDER: TokenHolder> {
-    fn convert_to_tokens(&self) -> Vec<Token<HOLDER>>;
-}
-
-impl<HOLDER: TokenHolder, const T: usize> Tokens<HOLDER> for &[Token<HOLDER>; T] {
-    fn convert_to_tokens(&self) -> Vec<Token<HOLDER>> {
-        self.to_vec()
-    }
-}
-
-impl<HOLDER: TokenHolder> Tokens<HOLDER> for &str {
-    fn convert_to_tokens(&self) -> Vec<Token<HOLDER>> {
-        let mut tokens: Vec<Token<HOLDER>> = Vec::new();
-        self.chars().for_each(|char| {
-            tokens.push(Token::Char(char));
-        });
-        tokens
-    }
-}
-
-
-impl<HOLDER: TokenHolder> Tokens<HOLDER> for String {
-    fn convert_to_tokens(&self) -> Vec<Token<HOLDER>> {
-        let mut tokens: Vec<Token<HOLDER>> = Vec::new();
-        self.chars().for_each(|char| {
-            tokens.push(Token::Char(char));
-        });
-        tokens
-    }
-}
-
-#[derive(Default)]
-pub struct TokenBuilder<HOLDER: TokenHolder> {
-    pub tokens: LinkedList<Token<HOLDER>>,
-}
-
-impl<HOLDER: TokenHolder> TokenBuilder<HOLDER> {
-    pub fn new() -> TokenBuilder<HOLDER> {
-        TokenBuilder {
-            tokens: LinkedList::default()
-        }
-    }
-
-    pub fn push_custom(&mut self, tok: HOLDER) {
-        self.tokens.push_back(Token::Custom(tok))
-    }
-
-    pub fn from_file(&mut self, path: impl Into<String>) {
-        let lines = read_lines(path);
-        for line in lines {
-            line.chars().for_each(|ch| {
-                self.tokens.push_back(Token::Char(ch));
-            });
-            self.tokens.push_back(Token::NewLine);
-        }
-        self.tokens.push_back(Token::EOF);
-    }
-
-    pub fn found(&mut self, pattern: impl Tokens<HOLDER>) -> Vec<(usize, usize)> {
-        let mut start = None;
-        let pattern = pattern.convert_to_tokens();
-        let pattern: Vec<&Token<HOLDER>> = pattern.iter().collect();
-        let mut temp = 0usize;
-        let mut indexes: Vec<(usize, usize)> = Vec::new();
-        self.tokens.iter().enumerate().for_each(|(idx, x)| {
-            if start.is_none() {
-                if pattern[0] == x {
-                    if pattern.len() == 1 {
-                        indexes.push((idx, idx));
-                        start = None;
-                        temp = 0;
-                    }else {
-                        start = Some(idx);
-                        temp += 1;
-                    }
-                }
+    let range = tkb.found_range(Token::Custom(TestCustomTokens::CompileOnly),
+                                Token::Custom(TestCustomTokens::Group(TokenArgs::new()))
+    );
+    tkb.replace(range.as_slice(), |mut list| {
+        if let Token::Custom(TestCustomTokens::Group(a)) = &list[1] {
+            let a = a[0].to_text();
+            let args: Vec<&str> = a.split(":").collect();
+            if args.len() == 3 {
+                Token::Custom(TestCustomTokens::CompileOnlyCompound(
+                    args[0].to_string(),
+                    args[1].to_string(),
+                    args[2].to_string(),
+                ))
             } else {
-                if pattern[temp] == x {
-                    temp += 1;
-                } else {
-                    start = None;
-                    temp = 0;
-                }
-                if pattern.len() - 1 < temp {
-                    indexes.push((start.unwrap(), idx));
-                    start = None;
-                    temp = 0;
-                }
+                Token::Custom(TestCustomTokens::FailedCompound)
             }
-        });
-        indexes
-    }
-
-    pub fn replace(&mut self, slices: &[(usize, usize)], eq: Token<HOLDER>) {
-        //a -> b -> c -> d -> e -> f -> g = (2, 4) , (6, 7)
-        //a -> b -> || c -> d -> e -> f -> g
-        //a -> b -> MAP -> f -> g = (2, 4), (dx: 3, dy: 4) -3!
-        let mut negative_offset = 0usize;
-        slices.iter().for_each(|x| {
-            if x.1 + 1 - negative_offset >= self.tokens.len() {
-                return;
-            }
-            let mut right = self.tokens.split_off(x.1 + 1 - negative_offset);
-            let slice = self.tokens.split_off(x.0 - negative_offset);
-            negative_offset += slice.len() - 1;
-            self.tokens.push_back(eq.clone());
-            self.tokens.append(&mut right);
-        });
-    }
-
-    pub fn debug(&self) {
-        self.tokens.iter().for_each(|tk| {
-            println!("{:?}", tk);
-        });
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum TestCustomToken {
-    Def,
-    MethodCall(String),
-}
-
-impl TokenHolder for TestCustomToken {}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum Token<T: TokenHolder> {
-    Empty,
-    Char(char),
-    StrLit(String),
-    IntLit(i32),
-    EOF,
-    NewLine,
-    Custom(T),
-}
-
-impl<T: TokenHolder> Token<T> {
-    pub fn to_text(&self) -> String {
-        match self {
-            Token::Empty => { String::from("") }
-            Token::Char(c) => { format!("{}", c) }
-            StrLit(a) => { String::from(a) }
-            Token::IntLit(i) => { format!("{}", i) }
-            Token::EOF => { String::from("<eof>") }
-            Token::NewLine => { String::from("\n") }
-            Token::Custom(o) => { format!("{:?}", o) }
+        } else {
+            Token::Custom(TestCustomTokens::FailedCompound)
         }
-    }
+    });
+    let aaa = els.elapsed();
+
+    //tkb.debug();
+    println!("Elapsed: {:?}", aaa);
+
+    tkb.tokens.iter().for_each(|x| {
+        if let Token::Custom(TestCustomTokens::CompileOnlyCompound
+                             (group, artifact, ver)) = x {
+            println!(" DEP {group} {artifact} {ver}");
+        }
+        if let Token::Custom(TestCustomTokens::FailedCompound) = x {
+            println!(" FAILED");
+        }
+    });
 }
---
+++ src/stacktoken/mod.rs
@@ -0,0 +1,278 @@
+use std::collections::LinkedList;
+use std::fmt::Debug;
+use std::ops::Index;
+use crate::stacktoken::TestCustomTokens::Group;
+use crate::utils::read_lines;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct TokenArgs<HOLDER: TokenHolder> {
+    pub tokens: Vec<Token<HOLDER>>
+}
+
+impl<HOLDER: TokenHolder> TokenArgs<HOLDER> {
+
+    pub fn new() -> Self {
+        Self {
+            tokens: Vec::new()
+        }
+    }
+
+    pub fn from_list(list: LinkedList<Token<HOLDER>>) -> Self {
+        let mut tokens = Vec::new();
+        for tok in list {
+            tokens.push(tok);
+        }
+        Self {
+            tokens
+        }
+    }
+
+    pub fn remove_backs(&mut self) {
+        let tokens = self.tokens[1..self.tokens.len() - 1].to_vec();
+
+        self.tokens = tokens;
+    }
+
+    pub fn scan_string(&self) -> String {
+        let mut a: String = self.tokens.iter().map(|x| x.to_text().to_string()).collect();
+        a = a[1..a.len() - 1].to_string();
+        a
+    }
+
+}
+
+impl<HOLDER: TokenHolder> Index<usize> for TokenArgs<HOLDER> {
+    type Output = Token<HOLDER>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.tokens[index]
+    }
+}
+
+pub trait TokenHolder: Clone + Debug + PartialEq {}
+
+pub trait Tokens<HOLDER: TokenHolder> {
+    fn convert_to_tokens(&self) -> Vec<Token<HOLDER>>;
+}
+
+impl<HOLDER: TokenHolder, const T: usize> Tokens<HOLDER> for &[Token<HOLDER>; T] {
+    fn convert_to_tokens(&self) -> Vec<Token<HOLDER>> {
+        self.to_vec()
+    }
+}
+
+impl<HOLDER: TokenHolder> Tokens<HOLDER> for &str {
+    fn convert_to_tokens(&self) -> Vec<Token<HOLDER>> {
+        let mut tokens: Vec<Token<HOLDER>> = Vec::new();
+        self.chars().for_each(|char| {
+            tokens.push(Token::Char(char));
+        });
+        tokens
+    }
+}
+
+
+impl<HOLDER: TokenHolder> Tokens<HOLDER> for String {
+    fn convert_to_tokens(&self) -> Vec<Token<HOLDER>> {
+        let mut tokens: Vec<Token<HOLDER>> = Vec::new();
+        self.chars().for_each(|char| {
+            tokens.push(Token::Char(char));
+        });
+        tokens
+    }
+}
+
+#[derive(Default)]
+pub struct TokenBuilder<HOLDER: TokenHolder> {
+    pub tokens: LinkedList<Token<HOLDER>>,
+}
+
+impl<HOLDER: TokenHolder> TokenBuilder<HOLDER> {
+    pub fn new() -> TokenBuilder<HOLDER> {
+        TokenBuilder {
+            tokens: LinkedList::default()
+        }
+    }
+
+    pub fn push_custom(&mut self, tok: HOLDER) {
+        self.tokens.push_back(Token::Custom(tok))
+    }
+
+    pub fn from_file(&mut self, path: impl Into<String>) {
+        let lines = read_lines(path);
+        for line in lines {
+            line.chars().for_each(|ch| {
+                self.tokens.push_back(Token::Char(ch));
+            });
+            self.tokens.push_back(Token::NewLine);
+        }
+        self.tokens.push_back(Token::EOF);
+    }
+
+    pub fn found(&self, pattern: impl Tokens<HOLDER>) -> Vec<(usize, usize)> {
+        let mut start = None;
+        let pattern = pattern.convert_to_tokens();
+        let pattern: Vec<&Token<HOLDER>> = pattern.iter().collect();
+        let mut temp = 0usize;
+        let mut indexes: Vec<(usize, usize)> = Vec::new();
+        self.tokens.iter().enumerate().for_each(|(idx, x)| {
+            if start.is_none() {
+                if pattern[0] == x {
+                    if pattern.len() == 1 {
+                        indexes.push((idx, idx));
+                        start = None;
+                        temp = 0;
+                    }else {
+                        start = Some(idx);
+                        temp += 1;
+                    }
+                }
+            } else {
+                if pattern[temp] == x {
+                    temp += 1;
+                } else {
+                    start = None;
+                    temp = 0;
+                }
+                if pattern.len() - 1 < temp {
+                    indexes.push((start.unwrap(), idx));
+                    start = None;
+                    temp = 0;
+                }
+            }
+        });
+        indexes
+    }
+
+    pub fn found_range(&self, start: Token<HOLDER>, end: Token<HOLDER>) -> Vec<(usize, usize)> {
+        let mut indexes: Vec<(usize, usize)> = Vec::new();
+        let mut start_idx:Option<usize> = None;
+        let mut cur = 0;
+        for token in &self.tokens {
+            match start_idx {
+                None => {
+                    if token == &start {
+                        start_idx = Some(cur);
+                    }
+                }
+                Some(start) => {
+                    if token == &end {
+                        start_idx = None;
+                        indexes.push((start, cur));
+                    }
+                }
+            }
+            cur += 1;
+        }
+        indexes
+    }
+
+    pub fn replace(&mut self, slices: &[(usize, usize)], eq: fn (TokenArgs<HOLDER>) -> Token<HOLDER>) {
+        //a -> b -> c -> d -> e -> f -> g = (2, 4) , (6, 7)
+        //a -> b -> || c -> d -> e -> f -> g
+        //a -> b -> MAP -> f -> g = (2, 4), (dx: 3, dy: 4) -3!
+        let mut negative_offset = 0usize;
+        slices.iter().for_each(|x| {
+            if x.1 + 1 - negative_offset >= self.tokens.len() {
+                return;
+            }
+            let mut right = self.tokens.split_off(x.1 + 1 - negative_offset);
+            let slice = self.tokens.split_off(x.0 - negative_offset);
+            negative_offset += slice.len() - 1;
+            self.tokens.push_back(eq(TokenArgs::from_list(slice)));
+            self.tokens.append(&mut right);
+        });
+    }
+
+    pub fn debug(&self) {
+        self.tokens.iter().for_each(|tk| {
+            println!("{:?}", tk);
+        });
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum TestCustomTokens {
+    StartParen,
+    EndParen,
+    CompileOnly,
+    CompileOnlyCompound(String, String, String),
+    FailedCompound,
+    Group(TokenArgs<TestCustomTokens>)
+}
+
+impl PartialEq for TestCustomTokens {
+    fn eq(&self, other: &Self) -> bool {
+        match self {
+            TestCustomTokens::StartParen => {
+                if let TestCustomTokens::StartParen = other {
+                    true
+                } else {
+                    false
+                }
+            }
+            TestCustomTokens::EndParen => {
+                if let TestCustomTokens::EndParen = other {
+                    true
+                } else {
+                    false
+                }
+            }
+            TestCustomTokens::CompileOnly => {
+                if let TestCustomTokens::CompileOnly = other {
+                    true
+                } else {
+                    false
+                }
+            }
+            TestCustomTokens::FailedCompound => {
+                if let TestCustomTokens::FailedCompound = other {
+                    true
+                } else {
+                    false
+                }
+            }
+            TestCustomTokens::CompileOnlyCompound(_, _, _) => {
+                false
+            }
+            TestCustomTokens::Group(_) => {
+                if let Group(_) = other {
+                    true
+                } else {
+                    false
+                }
+            }
+        }
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        return !self.eq(other)
+    }
+}
+
+impl TokenHolder for TestCustomTokens {}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token<T: TokenHolder> {
+    Empty,
+    Char(char),
+    StrLit(String),
+    IntLit(i32),
+    EOF,
+    NewLine,
+    Custom(T),
+}
+
+impl<T: TokenHolder> Token<T> {
+    pub fn to_text(&self) -> String {
+        match self {
+            Token::Empty => { String::from("") }
+            Token::Char(c) => { format!("{}", c) }
+            Token::StrLit(a) => { String::from(a) }
+            Token::IntLit(i) => { format!("{}", i) }
+            Token::EOF => { String::from("<eof>") }
+            Token::NewLine => { String::from("\n") }
+            Token::Custom(o) => { format!("{:?}", o) }
+        }
+    }
+}
